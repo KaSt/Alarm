016ad92389aab0eb5ba95fe92817bad3721a8bf5
Author Ka <k@ka.st>
2015-04-29 12:05:04 +0200

    Initial commit

---
+++ app/src/main/java/st/ka/alarm/MainActivity.java
@@ -0,0 +1,63 @@
+package st.ka.alarm;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+
+	private static String SENT = "SMS_SENT";
+	private static String DELIVERED = "SMS_DELIVERED";
+	private static int MAX_SMS_MESSAGE_LENGTH = 160;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void sendSMS(String phoneNumber, String message) {
+
+		PendingIntent piSent = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+		PendingIntent piDelivered = PendingIntent.getBroadcast(this, 0,new Intent(DELIVERED), 0);
+		SmsManager smsManager = SmsManager.getDefault();
+
+		int length = message.length();
+		if(length > MAX_SMS_MESSAGE_LENGTH) {
+			ArrayList<String> messagelist = smsManager.divideMessage(message);
+			smsManager.sendMultipartTextMessage(phoneNumber, null, messagelist, null, null);
+		}
+		else
+			smsManager.sendTextMessage(phoneNumber, null, message, piSent, piDelivered);
+	}
+}
+
